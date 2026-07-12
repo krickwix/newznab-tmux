77e05f65b468fbf73b2b0d4ec7627f9fccf96929
Fabien Andrieux <krickwix@gmail.com>
2026-07-13 01:05:57 +0200

    test(deploy): pin incomplete release v88

--- tests/Unit/Services/Metrics/NntmuxDeploymentManifestTest.php
+++ tests/Unit/Services/Metrics/NntmuxDeploymentManifestTest.php
@@ -260,11 +260,11 @@ final class NntmuxDeploymentManifestTest extends TestCase
         self::assertIsArray($orchestrator);
         self::assertSame(1, $orchestrator['spec']['replicas'] ?? null);
         self::assertStringEndsWith(
-            ':microservices-pods-20260713-exact-release-cohort-v87',
+            ':microservices-pods-20260713-incomplete-release-v88',
             (string) ($orchestrator['spec']['template']['spec']['containers'][0]['image'] ?? ''),
         );
         self::assertSame(
-            'microservices-pods-20260713-exact-release-cohort-v87',
+            'microservices-pods-20260713-incomplete-release-v88',
             $environment($orchestrator)['NNTMUX_BUILD_VERSION'] ?? null,
         );
         self::assertNotContains('--shadow', $orchestrator['spec']['template']['spec']['containers'][0]['args'] ?? []);
