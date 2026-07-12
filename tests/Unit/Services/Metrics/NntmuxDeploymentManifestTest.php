dc9fc2ff9c2d769870f14245f7fcc9cc58193296
Fabien Andrieux <krickwix@gmail.com>
2026-07-13 01:29:52 +0200

    test(deploy): pin release lineage v89

--- tests/Unit/Services/Metrics/NntmuxDeploymentManifestTest.php
+++ tests/Unit/Services/Metrics/NntmuxDeploymentManifestTest.php
@@ -260,11 +260,11 @@ final class NntmuxDeploymentManifestTest extends TestCase
         self::assertIsArray($orchestrator);
         self::assertSame(1, $orchestrator['spec']['replicas'] ?? null);
         self::assertStringEndsWith(
-            ':microservices-pods-20260713-incomplete-release-v88',
+            ':microservices-pods-20260713-release-lineage-v89',
             (string) ($orchestrator['spec']['template']['spec']['containers'][0]['image'] ?? ''),
         );
         self::assertSame(
-            'microservices-pods-20260713-incomplete-release-v88',
+            'microservices-pods-20260713-release-lineage-v89',
             $environment($orchestrator)['NNTMUX_BUILD_VERSION'] ?? null,
         );
         self::assertNotContains('--shadow', $orchestrator['spec']['template']['spec']['containers'][0]['args'] ?? []);
